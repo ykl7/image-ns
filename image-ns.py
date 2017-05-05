bc87504a17fffa7e27c9ae35e0adb2e08f8e93e6
ykl7 <yklal95@gmail.com>
2017-05-06 00:16:58 +0530

    basic init

---
+++ image-ns.py
@@ -0,0 +1,4 @@
+VGG_PATH = 'vgg-imagenet-19.mat'
+
+if __name__ == '__main__':
+    main()
---
+++ vggnet.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+import scipy.io
+
+VGG19_LAYERS = (
+    'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
+    'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
+    'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
+    'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+    'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
+    'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
+    'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
+    'relu5_3', 'conv5_4', 'relu5_4'
+)
+
+def _convolution_layer(input, weights, bias):
+    convolution = tf.nn.conv2d(input, tf.constant(weights), strides=(1, 1, 1, 1), padding='SAME')
+    return tf.nn.bias_add(convolution, bias)
+
+def load_network(path):
+    data = scipy.io.loadmat(path)
+    mean = data['normalization'][0][0][0]
+    mean_pixel = np.mean(mean, axis=(0, 1))
+    weights = data['layers'][0]
+    return weights, mean_pixel
+
+def normalize(image, mean_pixel):
+    return image - mean_pixel
+
+def retrieve_original(image, mean_pixel):
+    return image + mean_pixel
