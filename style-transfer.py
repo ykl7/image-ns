fc41d68d327616faf46ab5da680bec3ac3f4266c
ykl7 <yklal95@gmail.com>
2017-05-08 00:55:40 +0530

    dimensionality for style transfer

---
+++ style-transfer.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+from PIL import Image
+from functools import reduce
+from operator import mul
+
+import vggnet
+
+CONTENT_LAYERS = ('relu4_2', 'relu5_2')
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+
+def rgb2grayscale(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def grayscale2rgb(grayscale):
+    w, h = grayscale.shape
+    rgbrep = np.empty((w, h, 3), dtype=np.float32)
+    rgbrep[:, :, 2] = rgbrep[:, :, 1] = rgbrep[:, :, 0] = grayscale
+    return rgbrep
+
+def _calc_tensor_size(tensor):
+    return reduce(mul, (dim.value for dim in tensor.get_shape()), 1)
+
+def style_transfer(neural_net, content, styles):
+	overall_shape = (1,) + content.shape
+    per_style_shape = [(1,) + style.shape for style in styles]
+    content_features = {}
+    style_features = [{} for _ in styles]
+
+    vgg_network_weights, vgg_network_mean_pixel = vgg.load_network(neural_net)
