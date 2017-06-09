c0cb5b447830a42acfabb1e20fd42929d6e2b027
ykl7 <yklal95@gmail.com>
2017-06-09 15:02:42 +0530

    content and style loss computation

--- style-transfer.py
+++ style-transfer.py
@@ -22,7 +22,9 @@ def grayscale2rgb(grayscale):
 def _calc_tensor_size(tensor):
     return reduce(mul, (dim.value for dim in tensor.get_shape()), 1)
 
-def style_transfer(neural_net, content, styles, style_layer_weight_exponent, pooling, initial, initial_noiseblend):
+def style_transfer(neural_net, content, styles, style_layer_weight_exponent, pooling, initial, initial_noiseblend,
+    content_weight, content_weight_blend, style_blend_weights, style_weight):
+
 	overall_shape = (1,) + content.shape
     per_style_shape = [(1,) + style.shape for style in styles]
     content_features = {}
@@ -84,3 +86,31 @@ def style_transfer(neural_net, content, styles, style_layer_weight_exponent, poo
             initial = (initial) * initial_content_noise_coeff + (tf.random_normal(shape) * 0.256) * (1.0 - initial_content_noise_coeff)
         image = tf.Variable(initial)
         net = vggnet.preloaded_network(vgg_network_weights, image, pooling)
+
+        # content loss computations
+
+        content_layers_weights = {}
+        content_layers_weights['relu4_2'] = content_weight_blend
+        content_layers_weights['relu5_2'] = 1.0 - content_weight_blend
+
+        content_loss = 0
+        all_content_losses = []
+        for content_layer in CONTENT_LAYERS:
+            all_content_losses.append(content_layers_weights[content_layer] * content_weight * (2 * 
+                tf.nn.l2_loss(net[content_layer] - content_features[content_layer]) / content_features[content_layer].size))
+        content_loss += reduce(tf.add, all_content_losses)
+
+        # style loss computations
+
+        style_loss = 0
+        for i in range(len(styles)):
+            all_style_losses = []
+            for style_layer in STYLE_LAYERS:
+                layer = net[style_layer]
+                _, height, width, number = map(lambda i: i.value, layer.get_shape())
+                size = height * width * number
+                features = tf.reshape(layer, (-1, number))
+                gram = tf.matmul(tf.transpose(features), features) / size
+                style_gram = style_features[i][style_layer]
+                style_losses.append(style_layers_weights[style_layer] * 2 * tf.nn.l2_loss(gram - style_gram) / style_gram.size)
+            style_loss += style_weight * style_blend_weights[i] * reduce(tf.add, all_style_losses)
